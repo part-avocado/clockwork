21fd1d74080779ed4e5f530ff29c53b15ca3a88f
Penguin Studios <89282211+part-avocado@users.noreply.github.com>
2025-02-13 15:17:28 -0500

    Initial commit

---
+++ clockwork/clockworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockworkApp.swift
+//  clockwork
+//
+//  Created by James on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct clockworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
